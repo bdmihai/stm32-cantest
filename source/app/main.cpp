a9a517d5b45b6433e91c0a467fcd8a7a4091cfc8
Mihai <bdmihai@gmail.com>
2023-12-15 23:21:49 +0100

    update cpyrght

--- source/app/main.cpp
+++ source/app/main.cpp
@@ -1,6 +1,6 @@
 /*_____________________________________________________________________________
  │                                                                            |
- │ COPYRIGHT (C) 2022 Mihai Baneu                                             |
+ │ COPYRIGHT (C) 2021 Mihai Baneu                                             |
  │                                                                            |
  | Permission is hereby  granted,  free of charge,  to any person obtaining a |
  | copy of this software and associated documentation files (the "Software"), |
@@ -23,7 +23,7 @@
  |                                                                            |
  |  Author: Mihai Baneu                           Last modified: 15.Dec.2023  |
  |                                                                            |
- |___________________________________________________________________________*/
+ |___________________________________________________________________________*
 
 #include "stm32f4xx.h"
 #include "stm32rtos.h"
