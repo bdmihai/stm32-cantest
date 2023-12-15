dde75c066cac3fd97f1cd2e9e11f12801bc071e2
Mihai <bdmihai@gmail.com>
2023-12-15 23:20:05 +0100

    blink setup

---
+++ source/app/gpio.cpp
@@ -0,0 +1,55 @@
+/*_____________________________________________________________________________
+ │                                                                            |
+ │ COPYRIGHT (C) 2022 Mihai Baneu                                             |
+ │                                                                            |
+ | Permission is hereby  granted,  free of charge,  to any person obtaining a |
+ | copy of this software and associated documentation files (the "Software"), |
+ | to deal in the Software without restriction,  including without limitation |
+ | the rights to  use, copy, modify, merge, publish, distribute,  sublicense, |
+ | and/or sell copies  of  the Software, and to permit  persons to  whom  the |
+ | Software is furnished to do so, subject to the following conditions:       |
+ |                                                                            |
+ | The above  copyright notice  and this permission notice  shall be included |
+ | in all copies or substantial portions of the Software.                     |
+ |                                                                            |
+ | THE SOFTWARE IS PROVIDED  "AS IS",  WITHOUT WARRANTY OF ANY KIND,  EXPRESS |
+ | OR   IMPLIED,   INCLUDING   BUT   NOT   LIMITED   TO   THE  WARRANTIES  OF |
+ | MERCHANTABILITY,  FITNESS FOR  A  PARTICULAR  PURPOSE AND NONINFRINGEMENT. |
+ | IN NO  EVENT SHALL  THE AUTHORS  OR  COPYRIGHT  HOLDERS  BE LIABLE FOR ANY |
+ | CLAIM, DAMAGES OR OTHER LIABILITY,  WHETHER IN AN ACTION OF CONTRACT, TORT |
+ | OR OTHERWISE, ARISING FROM,  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR  |
+ | THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                 |
+ |____________________________________________________________________________|
+ |                                                                            |
+ |  Author: Mihai Baneu                           Last modified: 05.Oct.2022  |
+ |                                                                            |
+ |___________________________________________________________________________*/
+ 
+#include "stm32f4xx.h"
+#include "stm32rtos.h"
+#include "queue.h"
+#include "gpio.h"
+
+void gpio_init()
+{
+    /* disable JTAG */
+    MODIFY_REG(GPIOA->MODER,    GPIO_MODER_MODER15_Msk,     0);                         /* JTDI disabled */
+    MODIFY_REG(GPIOB->MODER,    GPIO_MODER_MODER4_Msk,      0);                         /* NJTRST disabled */
+    MODIFY_REG(GPIOB->MODER,    GPIO_MODER_MODER3_Msk,      0);                         /* JTDO disabled */
+
+    /* configure LED pin */
+    MODIFY_REG(GPIOB->MODER,    GPIO_MODER_MODER0_Msk,     GPIO_MODER_MODER0_0);        /* set the pin as output */
+    MODIFY_REG(GPIOB->OTYPER,   GPIO_OTYPER_OT0_Msk,        0);                         /* push pull */
+    MODIFY_REG(GPIOB->OSPEEDR,  GPIO_OSPEEDR_OSPEED0_Msk,   0);                         /* low speed */
+    MODIFY_REG(GPIOB->PUPDR,    GPIO_PUPDR_PUPD0_Msk,       0);                         /* no pull up, no pull down */
+}
+
+void gpio_set_led()
+{
+    GPIOB->BSRR = GPIO_BSRR_BS0;
+}
+
+void gpio_reset_led()
+{
+    GPIOB->BSRR = GPIO_BSRR_BR0;
+}
---
+++ source/app/system.cpp
@@ -0,0 +1,107 @@
+/*_____________________________________________________________________________
+ │                                                                            |
+ │ COPYRIGHT (C) 2021 Mihai Baneu                                             |
+ │                                                                            |
+ | Permission is hereby  granted,  free of charge,  to any person obtaining a |
+ | copy of this software and associated documentation files (the "Software"), |
+ | to deal in the Software without restriction,  including without limitation |
+ | the rights to  use, copy, modify, merge, publish, distribute,  sublicense, |
+ | and/or sell copies  of  the Software, and to permit  persons to  whom  the |
+ | Software is furnished to do so, subject to the following conditions:       |
+ |                                                                            |
+ | The above  copyright notice  and this permission notice  shall be included |
+ | in all copies or substantial portions of the Software.                     |
+ |                                                                            |
+ | THE SOFTWARE IS PROVIDED  "AS IS",  WITHOUT WARRANTY OF ANY KIND,  EXPRESS |
+ | OR   IMPLIED,   INCLUDING   BUT   NOT   LIMITED   TO   THE  WARRANTIES  OF |
+ | MERCHANTABILITY,  FITNESS FOR  A  PARTICULAR  PURPOSE AND NONINFRINGEMENT. |
+ | IN NO  EVENT SHALL  THE AUTHORS  OR  COPYRIGHT  HOLDERS  BE LIABLE FOR ANY |
+ | CLAIM, DAMAGES OR OTHER LIABILITY,  WHETHER IN AN ACTION OF CONTRACT, TORT |
+ | OR OTHERWISE, ARISING FROM,  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR  |
+ | THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                 |
+ |____________________________________________________________________________|
+ |                                                                            |
+ |  Author: Mihai Baneu                           Last modified: 15.Dec.2023  |
+ |                                                                            |
+ |___________________________________________________________________________*
+
+#include "stm32f4xx.h"
+#include "stm32rtos.h"
+#include "gpio.h"
+#include "system.h"
+
+void system_init()
+{
+    /* configure Flash prefetch, Instruction cache, Data cache */ 
+    SET_BIT(FLASH->ACR, FLASH_ACR_ICEN | FLASH_ACR_DCEN | FLASH_ACR_PRFTEN);
+
+    /* configure the main internal regulator output voltage */
+    SET_BIT(RCC->APB1ENR, RCC_APB1ENR_PWREN);
+    MODIFY_REG(PWR->CR, PWR_CR_VOS_Msk, PWR_CR_VOS_1);
+
+    /* enable the external High Speed Clock @25MHz*/
+    SET_BIT(RCC->CR, RCC_CR_HSEON);
+    do {
+    } while ((RCC->CR & RCC_CR_HSERDY_Msk) != RCC_CR_HSERDY);
+
+    /* configure the PLL */
+    MODIFY_REG(RCC->PLLCFGR, RCC_PLLCFGR_PLLSRC_Msk, RCC_PLLCFGR_PLLSRC_HSE);
+    MODIFY_REG(RCC->PLLCFGR, RCC_PLLCFGR_PLLM_Msk, 8 << RCC_PLLCFGR_PLLM_Pos);
+    MODIFY_REG(RCC->PLLCFGR, RCC_PLLCFGR_PLLN_Msk, 384 << RCC_PLLCFGR_PLLN_Pos);
+    MODIFY_REG(RCC->PLLCFGR, RCC_PLLCFGR_PLLP_Msk, 1 << RCC_PLLCFGR_PLLP_Pos);
+    MODIFY_REG(RCC->PLLCFGR, RCC_PLLCFGR_PLLQ_Msk, 8 << RCC_PLLCFGR_PLLQ_Pos);
+    MODIFY_REG(RCC->PLLCFGR, RCC_PLLCFGR_PLLR_Msk, 2 << RCC_PLLCFGR_PLLR_Pos);
+
+    /* enable PLL clock generation */
+    SET_BIT(RCC->CR, RCC_CR_PLLON);
+    do {
+    } while ((RCC->CR & RCC_CR_PLLRDY_Msk) != RCC_CR_PLLRDY);
+
+    /* to correctly read data from FLASH memory, the number of wait states (LATENCY) 
+       must be correctly programmed according to the frequency of the CPU clock 
+       (HCLK) of the device. */
+    MODIFY_REG(FLASH->ACR, FLASH_ACR_LATENCY_Msk, FLASH_ACR_LATENCY_3WS);
+
+    /* set the highest APBx dividers in order to ensure that we do not go through
+        a non-spec phase whatever we decrease or increase HCLK. */
+    MODIFY_REG(RCC->CFGR, RCC_CFGR_PPRE1_Msk, RCC_CFGR_PPRE1_DIV16);
+    MODIFY_REG(RCC->CFGR, RCC_CFGR_PPRE2_Msk, RCC_CFGR_PPRE2_DIV16);
+    MODIFY_REG(RCC->CFGR, RCC_CFGR_HPRE_Msk, RCC_CFGR_HPRE_DIV1);
+
+    /* set the SYSCLK to PLL */
+    MODIFY_REG(RCC->CFGR, RCC_CFGR_SW_Msk, RCC_CFGR_SW_PLL);
+    do {
+    } while ((RCC->CFGR & RCC_CFGR_SWS_Msk) != RCC_CFGR_SWS_PLL);
+
+    /* disable HSI clock */
+    CLEAR_BIT(RCC->CR, RCC_CR_HSION);
+    do {
+    } while ((RCC->CR & RCC_CR_HSIRDY_Msk) == RCC_CR_HSIRDY);
+
+    /* confgure the APB clocks */
+    MODIFY_REG(RCC->CFGR, RCC_CFGR_PPRE1_Msk, RCC_CFGR_PPRE1_DIV2);
+    MODIFY_REG(RCC->CFGR, RCC_CFGR_PPRE2_Msk, RCC_CFGR_PPRE2_DIV2);
+
+    /* enable AHB1 ports clock */
+    SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_GPIOAEN);
+    SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_GPIOBEN);
+    SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_GPIOCEN);
+    SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_GPIOHEN);
+
+    /* one us timer for delay */
+    TIM10->PSC = (configCPU_CLOCK_HZ / 1000000) - 1;
+    MODIFY_REG(TIM10->CR1, TIM_CR1_CEN_Msk, TIM_CR1_CEN);
+
+    /* stop timer when debuggng */
+    SET_BIT(DBGMCU->APB2FZ, DBGMCU_APB2_FZ_DBG_TIM10_STOP);
+}
+
+/**
+ * delay in us (blockig)
+ */
+extern "C" void delay_us(const uint32_t us)
+{
+    SET_BIT(TIM10->EGR, TIM_EGR_UG);
+    do {
+    } while (TIM10->CNT < us );
+}
